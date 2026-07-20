8d6911188b93e92cad2875c848720654c7bda04f
viweb25 <admin@viwebsync.com>
2026-07-20 20:08:06 +0530

    build error fix

--- components/pages/Project/ProjectPage.tsx
+++ components/pages/Project/ProjectPage.tsx
@@ -2,14 +2,14 @@ import RkProjectsPage from "./RkProjectPage";
 import TimberProjectsPage from "./TimberProjectPage";
 
 interface ProjectsPageProps {
-  header: any;
-  footer: any;
+  header?: any;
+  footer?: any;
   theme: any;
   data: any;
 }
 
 export default function ProjectsPage(props: ProjectsPageProps) {
-  switch (props.data.variant) {
+  switch (props.data?.variant) {
     case "timber":
       return <TimberProjectsPage {...props} />;
 
