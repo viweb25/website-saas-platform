6ac25acf9e0db82926f2b2f90c75e6859720375b
viweb25 <admin@viwebsync.com>
2026-07-20 20:20:18 +0530

    platform update

--- components/pages/About/AboutPage.tsx
+++ components/pages/About/AboutPage.tsx
@@ -1,8 +1,15 @@
 import RkAboutPage from "./RkAboutPage";
 import TimberAboutPage from "./TimberAboutPage";
 
+interface AboutPageProps {
+  data: any;
+  theme: any;
+  header?: any;
+  footer?: any;
+}
+
 export default function AboutPage(props: AboutPageProps) {
-  switch (props.data.variant) {
+  switch (props.data?.variant) {
     case "timber":
       return <TimberAboutPage {...props} />;
 
