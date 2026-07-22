a3012e2fe4bba83d14681a07e0798ad3eac16169
viweb25 <admin@viwebsync.com>
2026-07-22 22:24:12 +0530

    rk service update

--- components/pages/About/RkAboutPage.tsx
+++ components/pages/About/RkAboutPage.tsx
@@ -12,7 +12,7 @@ type Props = {
 
 // Helper SVG Symbol Switcher
 function FeatureSymbol({ type, color }: { type: string; color: string }) {
-  const iconMap: Record<string, JSX.Element> = {
+  const iconMap: Record<string, React.ReactNode> = {
     blueprint: (
       <path strokeLinecap="round" strokeLinejoin="round" d="M9 12h6m-6 4h6m2 5H7a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5.586a1 1 0 0 1 .707.293l5.414 5.414a1 1 0 0 1 .293.707V19a2 2 0 0 1-2 2Z" />
     ),
